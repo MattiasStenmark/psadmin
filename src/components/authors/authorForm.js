13934190e70497d062b906c6062603121d7fa272
Mattias Stenmark <mattias.stenmark@thomascook.se>
2016-01-12 09:06:07 +0100

    added saved message after author save

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -12,13 +12,15 @@ var AuthorForm = React.createClass({
 					name="firstName"
 					label="First Name"
 					value={this.props.author.firstname}
-					onChange={this.props.onChange} />
+					onChange={this.props.onChange} 
+					error={this.props.errors.firstName} />
 
 				<Input
 					name="lastName"
 					label="Last Name"
 					value={this.props.author.lastname}
-					onChange={this.props.onChange} />
+					onChange={this.props.onChange}
+					error={this.props.errors.lastName} />
 				
 				<input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave} />
 			</form>
--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,6 +4,7 @@ var React = require('React');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
 	mixins: [
@@ -14,7 +15,8 @@ var ManageAuthorPage = React.createClass({
 
 	getInitialState: function() {
 		return {
-			author: {id: '', firstName: '', lastName: ''}
+			author: {id: '', firstName: '', lastName: ''},
+			errors: {}
 		};
 	
 	},
@@ -26,10 +28,36 @@ var ManageAuthorPage = React.createClass({
 		return this.setState({author: this.state.author});
 	},
 
+	authorFormIsValid: function() {
+		var formIsValid = true;
+		this.state.errors = {}; //clear any previous errors.
+
+		if (this.state.author.firstName.length < 3) {
+			this.state.errors.firstName = 'First name must be at least 3 characters';
+			formIsValid = false;
+		}
+
+		if (this.state.author.lastName.length < 3) {
+			this.state.errors.lastName = 'Last name must be at least 3 characters';
+			formIsValid = false;
+		}
+
+		this.setState({errors: this.state.errors});
+		return formIsValid;
+	},
+
 	saveAuthor: function(event) {
 		event.preventDefault();
+
+		if(!this.authorFormIsValid()) {
+			return;
+		}
+
+
 		AuthorApi.saveAuthor(this.state.author);
+		toastr.success('Author saved.');
 		this.transitionTo('authors');
+
 	},
 
 	render: function() {
@@ -37,7 +65,8 @@ var ManageAuthorPage = React.createClass({
 			<AuthorForm 
 				author={this.state.author} 
 				onChange={this.setAuthorState}
-				onSave={this.saveAuthor} />
+				onSave={this.saveAuthor} 
+				errors={this.state.errors} />
 
 		);
 	}
